3ac56f98eb5007249705afa092369c9f63598220
Thiago Paiva <paiva.thiago@gmail.com>
2022-08-09 11:12:14 +0100

    Structure changes.

---
+++ js/info.js
@@ -0,0 +1,39 @@
+const headerData = {
+    name:'Thiago Paiva',
+    desc:'Developer since 2005. Started studying computing as a hobby, when was a teenager, after some courses discovered the passion about finding faster ways to automatize processes and sharing the knowledge of the discoveries with the people. Worked in public and private sectors, likes Java and Loves JavaScript!',
+    PPT:'Professional Projects Timeline',
+    links:[
+       {ico:'fab fa-linkedin',title:'Linkedin',url:'https://www.linkedin.com/in/paivathiago'},              
+       {ico:'fab fa-github',title:'GitHub',url:'https://github.com/paiva-thiago'},
+       {ico:'fab fa-codepen',title:'Codepen',url:'https://codepen.io/paivacodes/'},
+       {ico:'fas fa-user-graduate',title:'Lattes',url:'http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4453321T3'},
+       {ico:'fas fa-rss',title:'PaivaWorks, my development blog! (soon!)',url:'http://paiva.works/'}
+    ]
+  }   
+const websiteData = {
+    dict: words,
+    ages: [
+        historyItem('08/2022','','Coder (Development Teacher)','Let\'s Code', 'Teaching Java-related courses.','Java','right' ),
+        historyItem('09/2021','','Sr. Software Developer', 'Integer Consulting', 'Working in development and maintenance of systems related to Portugal\'s Social Security.','Java5, Java6, EJB, Swing, Jenkins, SVN', 'left'),
+        historyItem('01/2022','02/2022','Content Producer','Trybe','Development of material for java spring framework course.','Java, Spring','right'),
+        placeItem('Lisbon/Remote'),
+        historyItem('03/2021','09-2021', 'Senior IT Analyst', 'CRECI-SP', 'Working in development and project of systems of the council.', '.NET, EF, nodeJs, PHP, C#, ASP, PHP','left'),
+        placeItem('São Paulo/Remote'),
+        historyItem('04/2018','03/2021', 'Sr. Systems Analyst', 'Prefeitura Municipal de São Bernardo do Campo/SP', 'Working in development of tax systems of the city hall.','Struts1, EJB1, Javascript, HTML, CSS.','right' ),
+        placeItem('São Bernardo do Campo'),
+        historyItem('01/2017','03/2018', 'Sr. Analyst Java EE', 'Meta', 'Working in a sustentation team at Meta, based on an important insurance company in São Paulo downtown. While acting on find solutions in a Car Insurance Budget Calculation System bugs, I\'ve developed some tools to help in this process.', 'JSF, EJB, JPA, RichFaces, Hibernate, Javascript, HTML, CSS.','left'),		  
+        placeItem('São Paulo'),  
+        eventItem('2016 Licensed in Professional Education Centro Paula Souza'),  
+        historyItem('04/2015','12/2016', 'Substitute Teacher', 'IFSP - Cubatão Campus','Teaching some development courses in web, like JavaScript, Jquery, HTML5, CSS3, and Java EE. Used to help on my classes some tools like LMS (Moodle) and Webquests.', 'Teaching JavaScript, Jquery, HTML5, CSS3, and Java EE. Using Moodle to student\'s homeworks.','right' ),		  
+        placeItem('Cubatão (as Teacher)'),  
+        historyItem('09/2014', '05/2016', 'Sr Programmer Analyst', 'Virtus Informática', '<ul><li>Maintenance of the ERP System for meal ticket companies.</li> <li>Migration project when one of our customer was sold, moving all data from our system to the new owner.</li><li>Migration from the old Delphi desktop system to a new version, in Java EE</li></ul> ',  'Delphi/Oracle/XML(Maintenance of Legacy System); Java/JPA/Hibernate/JSF/Primefaces ','left'),		  
+        historyItem('08/2012','08/2014', 'Sr Programmer Analyst', 'Virtus Informática ', 'Maintenance of the ERP System for meal ticket companies. ', 'Delphi/Oracle/XML','right'),
+        placeItem('Santos'),
+        eventItem('2010 Technological Aesthetics Specialist PUC/SP'),
+        historyItem('03/2008','08/2012', 'Pl Programmer Analyst', 'Virtus Informática - Cubatão/SP', 'Entered in a new project. A CMS and a Virtual Agency for a public sanitation authority. With the web agency, their users could get water bills or make order for water connections in a faster way than using telephones or getting in person. ',  'PHP/Oracle/Smarty Templates/HTML/Javascript','left'),
+        historyItem('06/2006','02/2008', 'Jr Programmer Analyst', 'Virtus Informática', 'Now as a permanent member of the team, started developing a web portal for meal ticket companies. That was a part of a solution composed by an ERP Desktop System and a web portal for the company\'s clients make the orders or consulting ticket statements. That was a huge increase in the solution value creating a more interative way of relationship with our clients.','PHP/Oracle/Smarty Templates/HTML/Javascript','right'),     
+        eventItem('2006 Technologist in Data Processing FATEC/BS'),
+        historyItem('08/2005','05/2006', 'Intern','Virtus Informática', 'Starting developing in a small IT consulting based in Cubatão. Entered in a Desktop System Maintanence team, starting creating and fixing reports.', 'Delphi/Oracle/QReport','left'),
+        placeItem('Cubatão')
+    ]
+  }
